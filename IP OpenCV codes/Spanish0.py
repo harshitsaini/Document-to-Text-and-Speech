4ce503df61c1e3ab43745f6a67926a3307b7e6ff
Harshit Saini <harshitsaini15@gmail.com>
2018-03-16 14:53:43 +0530

    Modified files '16-03-2018'

--- IP OpenCV codes/Spanish0.py
+++ IP OpenCV codes/Spanish0.py
@@ -3,32 +3,31 @@ import cv2
 from PIL import Image
 from pytesseract import *
 import numpy as np
-#src_path= 'C://Users//Harshit//Desktop//Initial_img//Machine Printed//Spanish0//'
-src_path= 'C://Users//Harshit//Desktop//'
-original = src_path+ "harshit.png"
 import pytesseract
+
+src_path= 'C://Users//Harshit//Desktop//Machine Learning//Image Analysis//Output Images//Machine Printed//Spanish Images//'
 #pytesseract.pytesseract.tesseract_cmd = 'H://Program Files (x86)//Tesseract-OCR//tesseract'
 pytesseract.pytesseract.tesseract_cmd = 'H:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
 TESSDATA_PREFIX='H://Program Files (x86)//Tesseract-OCR'
 tessdata_dir_config = '--tessdata-dir "H:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
 '''
-tessdata_dir_config = '--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
-# Example config: '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
-# It's important to include double quotes around the dir path.
-pytesseract.image_to_string(image, lang='chi_sim', config=tessdata_dir_config)
+    tessdata_dir_config = '--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
+    # Example config: '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+    # It's important to include double quotes around the dir path.
+    pytesseract.image_to_string(image, lang='chi_sim', config=tessdata_dir_config)
 '''
+
+ex_fl= "Spanish0//"
 
 def getThresholded(img,smooth_it):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     if smooth_it==True:
-        kernel = np.ones((7, 7), np.float32) / 49
+        kernel = np.ones((3, 3), np.float32) / 9
         smooth = cv2.filter2D(img, -1, kernel)
-        cv2.imwrite(src_path + "smoothened.png", smooth)
+        cv2.imwrite(src_path +ex_fl+ "smoothened.png", smooth)
         img=smooth
-    # ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
     threshed = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 121, 2)
-    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 11, 2)
-    cv2.imwrite(src_path + 'thresholded.png',threshed)
+    cv2.imwrite(src_path +ex_fl+ 'thresholded.png',threshed)
     return threshed
 
 def getClosed(img,iterations,kernel_size):
@@ -38,7 +37,7 @@ def getClosed(img,iterations,kernel_size):
     while (k != 0):
         closed = cv2.morphologyEx(closed, cv2.MORPH_CLOSE, kernel)
         k -= 1
-    cv2.imwrite(src_path + "closed.png", closed)
+    cv2.imwrite(src_path +ex_fl+ "closed.png", closed)
     return closed
 
 def getOpened(img,iterations,kernel_size):
@@ -48,18 +47,18 @@ def getOpened(img,iterations,kernel_size):
     while (k != 0):
         opened = cv2.morphologyEx(opened, cv2.MORPH_OPEN, kernel)
         k -= 1
-    cv2.imwrite(src_path + "opened.png", opened)
+    cv2.imwrite(src_path +ex_fl+ "opened.png", opened)
     return opened
 
 def getMorph(img,iterations,kernel_size,erode_it):
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
     if erode_it == True :
         eroded = cv2.erode(img, kernel, iterations=iterations)
-        cv2.imwrite(src_path + 'eroded.png', eroded)
+        cv2.imwrite(src_path +ex_fl+ 'eroded.png', eroded)
         return eroded
     else:
         dilated= cv2.dilate(img, kernel, iterations=iterations)
-        cv2.imwrite(src_path + "dilated.png", dilated)
+        cv2.imwrite(src_path +ex_fl+ "dilated.png", dilated)
         return dilated
 
 def img_show(*args):
@@ -68,18 +67,28 @@ def img_show(*args):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def get_string (img_path):
+def get_string0(img_path):
     img=cv2.imread(img_path)
-    conditioned = getThresholded(img,True)
-    #opened=getOpened(conditioned,50,12)
+    conditioned = getThresholded(img,False)#Dont know why i was smoothing my grayscale pic !!!???
     final =conditioned
-    cv2.imwrite(src_path+'final.png',final)
-    #img_show(img,conditioned,opened)
+    cv2.imwrite(src_path+ex_fl+'final.png',final)
     print('Conditioning Done !')
-    result= pytesseract.image_to_string(Image.open(src_path+'final.png'),lang='spa', config=tessdata_dir_config)
+    result= pytesseract.image_to_string(Image.open(src_path+ex_fl+'final.png'),lang='spa', config=tessdata_dir_config)
     #os.remove(temp)
     return result
 
 print('-----Start recognize text from image -----')
-print(get_string(src_path+ "harshit.png"))
-print("-------DONE-------")
+print(get_string0(src_path+"Spanish0//span_img.png"))
+print("-------DONE-------\n\n")
+
+ex_fl= "Spanish1//"
+
+print('-----Start recognize text from image -----')
+print(get_string0(src_path+"Spanish1//span1_img.png"))
+print("-------DONE-------\n\n")
+
+ex_fl= "Spanish2//"
+
+print('-----Start recognize text from image -----')
+print(get_string0(src_path+"Spanish2//span2_img.png"))
+print("-------DONE-------\n\n")
--- IP OpenCV codes/Spanish1.py
+++ IP OpenCV codes/Spanish1.py
@@ -3,48 +3,31 @@ import cv2
 from PIL import Image
 from pytesseract import *
 import numpy as np
-src_path= 'C://Users//Harshit//Desktop//Initial_img//Machine Printed//Spanish1//'
-
 import pytesseract
+
+src_path= 'C://Users//Harshit//Desktop//Machine Learning//Image Analysis//Output Images//Machine Printed//Spanish Images//Spanish1//'
 #pytesseract.pytesseract.tesseract_cmd = 'H://Program Files (x86)//Tesseract-OCR//tesseract'
 pytesseract.pytesseract.tesseract_cmd = 'H:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
 TESSDATA_PREFIX='H://Program Files (x86)//Tesseract-OCR'
 tessdata_dir_config = '--tessdata-dir "H:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
 '''
-tessdata_dir_config = '--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
-# Example config: '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
-# It's important to include double quotes around the dir path.
-pytesseract.image_to_string(image, lang='chi_sim', config=tessdata_dir_config)
-
+    tessdata_dir_config = '--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
+    # Example config: '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+    # It's important to include double quotes around the dir path.
+    pytesseract.image_to_string(image, lang='chi_sim', config=tessdata_dir_config)
 '''
 
 def get_string (img_path):
     img=cv2.imread(img_path)
-
-# IMAGE CONDITIONING
+    #print(img.dim)
+ # IMAGE CONDITIONING
     img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
  # THRESHOLDING
-    ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 115, 1)
-    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 11, 2)
+    #ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    #ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
     cv2.imwrite(src_path+'thresholded.png',img)
-
-    #################closing#########################
-    '''  kernel = np.ones((7, 7), np.uint8)
-    k = 40
-    closed = img
-    while (k != 0):
-        closed = cv2.morphologyEx(closed, cv2.MORPH_CLOSE, kernel);
-        k -= 1
-    cv2.imwrite(src_path + "closed.png", closed)'''
-    #################################################
-
-    ####################dilate#########################
-    '''kernel = np.ones((4, 4), np.uint8)
-    cv2.imwrite(src_path + "thres1.png",img)
-    dilated= cv2.dilate(img, kernel, iterations=2)
-    cv2.imwrite(src_path + "dilated.png", dilated)'''
-    ##################################################
 
     result= pytesseract.image_to_string(Image.open(src_path+'thresholded.png'),lang='spa', config=tessdata_dir_config)
     #kt=Image.open(src_path+"thres.png")
@@ -52,5 +35,5 @@ def get_string (img_path):
     return result
 
 print('-----Start recognize text from image -----')
-print(get_string(src_path+ "span1_img.jpg"))
+print(get_string(src_path+ "span1_img.png"))
 print("-------DONE-------")
