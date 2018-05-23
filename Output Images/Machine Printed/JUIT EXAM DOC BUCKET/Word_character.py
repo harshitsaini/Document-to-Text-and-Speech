e353d7e95c8e228414e065d3d25e1863c210f1e6
Harshit Saini <harshitsaini15@gmail.com>
2018-05-23 11:21:23 +0530

    TTS Update

--- Output Images/Machine Printed/JUIT EXAM DOC BUCKET/Word_character.py
+++ Output Images/Machine Printed/JUIT EXAM DOC BUCKET/Word_character.py
@@ -7,6 +7,7 @@ import numpy as np
 import pytesseract
 
 src_path= 'C://Users//Harshit//Desktop//Machine Learning//Image Analysis//Output Images//Machine Printed//JUIT EXAM DOC BUCKET//'
+#new_path= src_path
 #src_path= 'C://Users//Harshit//Desktop//Machine Learning//Image Analysis//Output Images//Machine Printed//'
 #pytesseract.pytesseract.tesseract_cmd = 'H://Program Files (x86)//Tesseract-OCR//tesseract'
 pytesseract.pytesseract.tesseract_cmd = 'H:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
@@ -19,11 +20,11 @@ def getThresholded(img,smooth_it):
     if smooth_it==True:
         kernel = np.ones((8, 8), np.float32) / 64
         smooth = cv2.filter2D(img, -1, kernel)
-        cv2.imwrite(new_path+ "smoothened.png", smooth)
+        cv2.imwrite(new_path+ "//smoothened.png", smooth)
         img=smooth
     #threshed = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 151, 0)
     _,threshed = cv2.threshold(img,200,255,cv2.THRESH_BINARY)
-    cv2.imwrite(new_path+ 'thresholded.png',threshed)
+    cv2.imwrite(new_path+ '//thresholded.png',threshed)
     return threshed
 
 def getClosed(img,iterations,kernel_size):
@@ -33,7 +34,7 @@ def getClosed(img,iterations,kernel_size):
     while (k != 0):
         closed = cv2.morphologyEx(closed, cv2.MORPH_CLOSE, kernel)
         k -= 1
-    cv2.imwrite(new_path+ "closed.png", closed)
+    cv2.imwrite(new_path+ "//closed.png", closed)
     return closed
 
 def getOpened(img,iterations,kernel_size):
@@ -43,18 +44,18 @@ def getOpened(img,iterations,kernel_size):
     while (k != 0):
         opened = cv2.morphologyEx(opened, cv2.MORPH_OPEN, kernel)
         k -= 1
-    cv2.imwrite(new_path+ "opened.png", opened)
+    cv2.imwrite(new_path+ "//opened.png", opened)
     return opened
 
 def getMorph(img,iterations,kernel_size,erode_it):
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
     if erode_it == True :
         eroded = cv2.erode(img, kernel, iterations=iterations)
-        cv2.imwrite(new_path+ 'eroded.png', eroded)
+        cv2.imwrite(new_path+ '//eroded.png', eroded)
         return eroded
     else:
         dilated= cv2.dilate(img, kernel, iterations=iterations)
-        cv2.imwrite(new_path+ "dilated.png", dilated)
+        cv2.imwrite(new_path+ "//dilated.png", dilated)
         return dilated
 
 def img_show(*args):
@@ -67,24 +68,36 @@ def horizontal_dilation(img,iterations,kernel_size):
     kernel= np.array([[0,0,0],[1,1,1],[0,0,0]])
 
     dilated = cv2.dilate(img, kernel, iterations=iterations)
-    cv2.imwrite(new_path + "dilated.png", dilated)
+    cv2.imwrite(new_path + "//dilated.png", dilated)
     return dilated
 
-def get_words(img_path):
+def get_words(img_path,img_id):
     ###########################CANNY EDGE DETECTION ###################################
-    img = cv2.imread(img_path)  ;        print(img.shape)
+
+    global new_path
+
+    img = cv2.imread(img_path+'//line'+str(img_id)+'.png')  ;        print(img.shape)
     img = cv2.resize(img, (1465,1737)) ; print(img.shape)
+
+    if not os.path.exists(img_path+'//line'+str(img_id)):
+    	os.makedirs(img_path+'//line'+str(img_id))
+
+    os.chdir(img_path+'//line'+str(img_id))
+    new_path= os.getcwd()
+
     thresholded= getThresholded(img,smooth_it=True)
     opened= getOpened(thresholded,iterations=1,kernel_size=3)
     edges = cv2.Canny(opened, 100, 200)
-    cv2.imwrite(new_path + 'canny.png', edges)
+    cv2.imwrite(new_path + '//canny.png', edges)
     #cv2.imshow('Edges',edges)
+
+
     ##############################DEFINING CONTOURS####################################
     conditioned = getMorph(img=edges, iterations=5, kernel_size=7, erode_it=False)
     final= conditioned
     #[a, contours, c] = cv2.findContours(final, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     [a, contours, c] = cv2.findContours(final, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print("No of text blocks detected are blocks :"+str(len(contours)))
+    print("No of text words detected are :"+str(len(contours)))
 
     #cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
     it=1
@@ -95,22 +108,24 @@ def get_words(img_path):
         imgROI = np.asarray(img[intY:intY+intH, intX:intX+intW])
         #cv2.imshow('block',imgROI)
         #intChar = cv2.waitKey(3500)
-        if not os.path.exists(new_path+'block'+str(it)+'//'):
-            os.makedirs(new_path+'block'+str(it)+'//')
-        cv2.imwrite(new_path+'block'+str(it)+'//'+'block.png', imgROI)
+        if not os.path.exists(new_path+'//word'+str(it)):
+            os.makedirs(new_path+'//word'+str(it))
+        cv2.imwrite(new_path+'//word'+str(it)+'//word.png', imgROI)
         it+=1    
     ###################################################################################
-    print('Blocks Extracted !')
+    print('Words Extracted !')
     return len(contours)
 
 
 
 if __name__ == "__main__":
 
+	global new_path
 
 	#####   Defining Working Directory  #####
 	old_cwd= os.getcwd()
 	new_cwd= src_path+'OS (T-1)2011//Overall Line Bucket//'
+	new_path= new_cwd
 
 	if not os.path.exists(new_cwd):
 		os.makedirs(new_cwd)
@@ -129,12 +144,14 @@ if __name__ == "__main__":
 
 	##### Extracting words from lines #####
 	for it in range(Max_line_number):
-		word_count= new_cwd+'line'+str(it+1)+'.png'
-		word_number_bucket.append(get_words(word_count)
+		line_path= new_cwd
+		word_number_bucket.append(get_words(img_path=line_path,img_id=it+1))
 
-	cnt=0
+	Total_words=0
 
-	Total_words= [cnt+=x for x in word_number_bucket]
+	for val in word_number_bucket:
+		Total_words+=val
+
 	print("Total number of words in whole document are:"+str(Total_words))
 
 
